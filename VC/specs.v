f7374c25e038c4e698181a51d47d4ea5b78bb8e1
Line Dejgaard <linedejgaard@gmail.com>
2025-03-10 16:03:18 +0100

    add specs

--- VC/specs.v
+++ VC/specs.v
@@ -5,19 +5,19 @@ From iris.prelude Require Import options.
 
 (** General (CAP-style) spec for a concurrent bag ("per-element spec") *)
 Record concurrent_bag {Σ} `{!heapGS Σ} := ConcurrentBag {
-  is_bag (P : val → iProp Σ) (s : val) : iProp Σ;
-  bag_pers (P : val → iProp Σ) (s : val) : Persistent (is_bag P s);
+  is_bag   (s : val) : iProp Σ;
+  bag_pers   (s : val) : Persistent (is_bag s);
   new_bag : val;
   bag_push : val;
   bag_pop : val;
-  mk_bag_spec (P : val → iProp Σ) :
+  mk_bag_spec   :
     {{{ True }}}
       new_bag #()
-    {{{ s, RET s; is_bag P s }}};
-  bag_push_spec (P : val → iProp Σ) s v :
-    {{{ is_bag P s ∗ P v }}} bag_push s v {{{ RET #(); True }}};
-  bag_pop_spec (P : val → iProp Σ) s :
-    {{{ is_bag P s }}} bag_pop s {{{ ov, RET ov; ⌜ov = NONEV⌝ ∨ ∃ v, ⌜ov = SOMEV v⌝ ∗ P v }}}
+    {{{ s, RET s; is_bag s }}};
+  bag_push_spec   s v :
+    {{{ is_bag s }}} bag_push s v {{{ RET #(); True }}};
+  bag_pop_spec   s :
+    {{{ is_bag s }}} bag_pop s {{{ ov, RET ov; ⌜ov = NONEV⌝ ∨ ∃ v, ⌜ov = SOMEV v⌝ }}}
 }.
 Global Arguments concurrent_bag _ {_}.
 
@@ -25,22 +25,22 @@ Global Arguments concurrent_bag _ {_}.
 
 
 Record concurrent_kmem {Σ} `{!heapGS Σ} := Concurrentkmem {
-  is_kmem (N : namespace) (P : list loc → iProp Σ) (s : val) : iProp Σ;
-  kmem_pers (N : namespace) (P : list loc → iProp Σ) (s : val) : Persistent (is_kmem N P s);
+  is_kmem (N : namespace)    (s : val) : iProp Σ;
+  kmem_pers (N : namespace)    (s : val) : Persistent (is_kmem N s);
   new_kmem : val;
   kmem_free : val;
   kmem_alloc : val;
-  new_kmem_spec (N : namespace) (P : list loc → iProp Σ) :
-    {{{ P [] }}} new_kmem #() {{{ v, RET v; is_kmem N P v }}};
-  kmem_free_spec (N : namespace) (P : list loc → iProp Σ) (Ψ : val → iProp Σ) s (v : loc) :
-    {{{ is_kmem N P s ∗ ∀ xs, P xs ={⊤ ∖ ↑ N}=∗ P (v :: xs) ∗ Ψ #()}}}
+  new_kmem_spec (N : namespace)    :
+    {{{ True }}} new_kmem #() {{{ v, RET v; is_kmem N v }}};
+  kmem_free_spec (N : namespace)    (Ψ : val → iProp Σ) s (v : loc) :
+    {{{ is_kmem N s ∗ Ψ #()}}}
       kmem_free s #v
     {{{ RET #(); Ψ #() }}};
-  kmem_alloc_spec (N : namespace) (P : list loc → iProp Σ) Ψ s :
-    {{{ is_kmem N P s ∗
-        (∀ v xs, P (v :: xs) ={⊤ ∖ ↑ N}=∗ P xs ∗ Ψ (SOMEV #v)) ∧
-        (P [] ={⊤ ∖ ↑ N}=∗ P [] ∗ Ψ NONEV) }}}
+  kmem_alloc_spec (N : namespace)  Ψ s :
+    {{{ is_kmem N s ∗
+        (Ψ (SOMEV #v)) ∧
+        (∗ Ψ NONEV) }}}
       kmem_alloc s
     {{{ v, RET v; Ψ v }}};
-}.
+}. 
 Global Arguments concurrent_kmem _ {_}.
