122efa87f2d84fc99a94bfac3b055f4f387f96ee
Line Dejgaard <linedejgaard@gmail.com>
2025-03-10 19:38:04 +0100

    add uncomment VST

--- VC/Verif_kalloc_Fun_Spec.v
+++ VC/Verif_kalloc_Fun_Spec.v
@@ -94,6 +94,13 @@
 (** ** Let's verify! *)
 
 Require VC.Preface. (* Check for the right version of VST *)
+(*Require Import VST.floyd.proofauto.
+Require Import VST.floyd.library.
+Require Import VC.kalloc.
+#[export] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
+Definition Vprog : varspecs.  mk_varspecs prog. Defined.
+Require Import VC.hints.  (* Import special hints for this tutorial. *)
+*)
 
 From iris.base_logic.lib Require Import invariants.
 From iris.program_logic Require Export weakestpre.
