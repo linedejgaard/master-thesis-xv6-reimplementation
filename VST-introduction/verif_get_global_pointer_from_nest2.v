c8bff6f5a39ac4b24135ac5cf5448540d8af3970
Line Dejgaard <linedejgaard@gmail.com>
2025-03-22 10:22:59 +0100

    ADD KALLOC_1_1 - THINK IT IS WRONG

--- VST-introduction/verif_get_global_pointer_from_nest2.v
+++ VST-introduction/verif_get_global_pointer_from_nest2.v
@@ -247,7 +247,23 @@ Definition kfree1_spec :=
          data_at sh (t_run) (original_freelist_pointer) new_head; (* the new head should point to the original freelist pointer *)
          data_at sh t_struct_kmem (Vint (Int.repr xx), new_head) (gv _kmem) (** the top of the freelist should point to the new head *)
          ).     
-       
+(* I THINK THIS IS WRONG, BECAUSE I DON'T THEY THEY ARE DISJOINT: *)
+Definition kfree1_1_spec := 
+ DECLARE _kfree1
+   WITH sh : share, new_head:val, original_freelist_pointer:val, xx:Z, gv:globals
+   PRE [ tptr tvoid]
+      PROP(writable_share sh; is_pointer_or_null original_freelist_pointer) (* writable_share is necessary *)
+      PARAMS (new_head) GLOBALS(gv)
+      SEP (data_at sh (t_run) nullval new_head * (* the input run struct should exists *)
+      data_at sh t_struct_kmem (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem) (* the kmem freelist should exists, xx is a placeholder for the spinlock *)
+      )
+   POST [ tvoid ]
+      PROP()
+      RETURN () (* no return value *)
+      SEP (
+         data_at sh (t_run) (original_freelist_pointer) new_head * (* the new head should point to the original freelist pointer *)
+         data_at sh t_struct_kmem (Vint (Int.repr xx), new_head) (gv _kmem) (** the top of the freelist should point to the new head *)
+         ).   
 
 (************************ alloc non_global *************************)
 
@@ -291,8 +307,8 @@ Definition kalloc1_spec := (* this doesn't assume that the list is empty, but th
       SEP (
          data_at sh (t_run) next original_freelist_pointer;
          data_at sh t_struct_kmem (Vint (Int.repr xx), next) (gv _kmem)
-         ).   
-       
+         ).
+
 
 (************************ pointer comparison *************************)
 Definition pointer_compare_0_spec :=
@@ -319,6 +335,17 @@ Definition pointer_compare_1_spec :=
             RETURN (Vint (if eq_dec p q then Int.one else Int.zero))
             SEP (data_at sh tint (Vint p_value) p; data_at sh tint (Vint q_value) q).
 
+Definition pointer_le_bool (pa_start pa_end : val) : bool :=
+   let comparison_result := 
+   force_val (sem_cast_i2i I32 Signed (force_val (sem_cmp_pp Cle pa_start pa_end))) in
+   match comparison_result with
+   | Vint z =>
+      if Z.eqb (Int.signed z) 1 then true
+      else if Z.eqb (Int.signed z) 0 then false
+      else false (* or you could raise an error, or have another default if the result is unexpected *)
+   | _ => false (* Or raise an error, or have another default if the result is unexpected *)
+   end.
+            
 Definition pointer_compare_2_spec :=
    DECLARE _pointer_compare_2
       WITH p: val, q:val, p_value:int, q_value:int, sh: share
@@ -371,7 +398,30 @@ Definition call_kfree1_if_1_spec :=
             data_at sh (t_run) (original_freelist_pointer) new_head *(* the new head should point to the original freelist pointer *)
             data_at sh t_struct_kmem (Vint (Int.repr xx), new_head) (gv _kmem) (** the top of the freelist should point to the new head *)
             ).
-         
+
+Definition freerange_no_loop_no_add_spec := 
+   DECLARE _freerange_no_loop_no_add
+      WITH sh : share, new_head:val, pa_end:val, original_freelist_pointer:val, xx:Z, gv:globals
+      PRE [ tptr tvoid, tptr tvoid]
+         PROP(writable_share sh; is_pointer_or_null original_freelist_pointer) 
+         PARAMS (new_head; pa_end) GLOBALS(gv)
+         SEP (
+            denote_tc_test_order new_head pa_end *
+            data_at sh (t_run) nullval new_head *
+            data_at sh t_struct_kmem (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem)
+         )
+      POST [ tvoid ]
+         PROP()
+         RETURN () (* no return value *)
+         SEP (
+            if (pointer_le_bool new_head pa_end) then
+            (data_at sh (t_run) nullval new_head * (* the input run struct should exists *)
+            data_at sh t_struct_kmem (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem) (* the kmem freelist should exists, xx is a placeholder for the spinlock *)
+            ) else 
+            (data_at sh (t_run) (original_freelist_pointer) new_head *(* the new head should point to the original freelist pointer *)
+            data_at sh t_struct_kmem (Vint (Int.repr xx), new_head) (gv _kmem) (** the top of the freelist should point to the new head *)
+         )).
+
 (************************ rounding *************************)
 
 Definition PGSIZE : Z := 4096. (** I think we should be able to retrieve this.. but it has to be greater than 0 *)
@@ -402,9 +452,11 @@ Definition Gprog : funspecs := [get_freelist1_input_spec;
 get_freelist1_input_spec'; get_freelist1_spec; get_i_spec; 
 get_xx_spec; get_freelist_spec;
 free_spec; free_spec'; alloc_spec; alloc_spec'; 
-kfree1_spec; kalloc1_spec; call_kfree1_spec; 
+kfree1_spec; kfree1_1_spec;
+kalloc1_spec; call_kfree1_spec; 
 call_kfree1_if_1_spec; pointer_compare_0_spec; pointer_compare_1_spec; 
-pointer_compare_2_spec; align_pointer_spec].
+pointer_compare_2_spec; 
+freerange_no_loop_no_add_spec; align_pointer_spec].
 
 
 Lemma body_get_freelist_input_spec:  semax_body Vprog Gprog f_get_freelist1_input get_freelist1_input_spec.
@@ -433,6 +485,9 @@ Proof. start_function. repeat forward. entailer!. Qed.
 
 Lemma body_kfree1: semax_body Vprog Gprog f_kfree1 kfree1_spec.
 Proof. start_function. repeat forward. entailer!. Qed.
+
+Lemma body_kfree1_1: semax_body Vprog Gprog f_kfree1 kfree1_1_spec.
+Proof. start_function. Intros. repeat forward. entailer!. Qed.
 
 Lemma body_alloc: semax_body Vprog Gprog f_alloc alloc_spec.
 Proof.
@@ -495,10 +550,14 @@ Proof. start_function. forward. Qed.
 Lemma body_pointer_compare_1: semax_body Vprog Gprog f_pointer_compare_1 pointer_compare_1_spec.
 Proof. start_function. forward. Qed.
 
-
-
 Lemma body_pointer_compare_2: semax_body Vprog Gprog f_pointer_compare_2 pointer_compare_2_spec.
 Proof. start_function. forward. Qed.
+
+
+Lemma body_freerange_no_loop_no_add: semax_body Vprog Gprog f_freerange_no_loop_no_add freerange_no_loop_no_add_spec.
+Proof. start_function. forward_if. 
+Admitted.
+
 
 (*Lemma body_align_pointer: semax_body Vprog Gprog f_align_pointer align_pointer_spec.
 Proof. start_function. forward. Admitted.*)
