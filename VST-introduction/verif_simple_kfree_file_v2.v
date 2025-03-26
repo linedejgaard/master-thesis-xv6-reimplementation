a7d7dcaf28ec31adb580d11440a13c1f4115c9f3
Line Dejgaard <linedejgaard@gmail.com>
2025-03-26 08:33:37 +0100

    verify with freelist and pointer comparison

--- VST-introduction/verif_simple_kfree_file_v2.v
+++ VST-introduction/verif_simple_kfree_file_v2.v
@@ -10,80 +10,6 @@ Definition t_run := Tstruct _run noattr.
 
 Definition t_struct_kmem := Tstruct _struct_kmem noattr.
 
-Definition kfree1_spec := 
- DECLARE _kfree1
-   WITH sh : share, new_head:val, original_freelist_pointer:val, xx:Z, gv:globals
-   PRE [ tptr tvoid]
-      PROP(writable_share sh; is_pointer_or_null original_freelist_pointer) 
-      PARAMS (new_head) GLOBALS(gv)
-      SEP (data_at sh (t_run) nullval new_head; 
-      data_at sh t_struct_kmem (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem) 
-      )
-   POST [ tvoid ]
-      PROP()
-      RETURN () 
-      SEP (
-         data_at sh (t_run) (original_freelist_pointer) new_head; 
-         data_at sh t_struct_kmem (Vint (Int.repr xx), new_head) (gv _kmem)
-         ).
-
-(* I THINK THIS IS WRONG, BECAUSE I DON'T THEY THEY ARE DISJOINT: *)
-Definition kfree1_1_spec := 
- DECLARE _kfree1
-   WITH sh : share, new_head:val, original_freelist_pointer:val, xx:Z, gv:globals
-   PRE [ tptr tvoid]
-      PROP(writable_share sh; is_pointer_or_null original_freelist_pointer) 
-      PARAMS (new_head) GLOBALS(gv)
-      SEP (data_at sh (t_run) nullval new_head * 
-      data_at sh t_struct_kmem (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem) 
-      )
-   POST [ tvoid ]
-      PROP()
-      RETURN ()
-      SEP (
-         data_at sh (t_run) (original_freelist_pointer) new_head * (* the new head should point to the original freelist pointer *)
-         data_at sh t_struct_kmem (Vint (Int.repr xx), new_head) (gv _kmem) (** the top of the freelist should point to the new head *)
-         ).
-
-
-Definition call_kfree1_spec := 
-   DECLARE _call_kfree1
-      WITH sh : share, new_head:val, original_freelist_pointer:val, xx:Z, gv:globals
-      PRE [ tptr tvoid]
-         PROP(writable_share sh; is_pointer_or_null original_freelist_pointer) (* writable_share is necessary *)
-         PARAMS (new_head) GLOBALS(gv)
-         SEP (data_at sh (t_run) nullval new_head; (* the input run struct should exists *)
-         data_at sh t_struct_kmem (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem) (* the kmem freelist should exists, xx is a placeholder for the spinlock *)
-         )
-      POST [ tvoid ]
-         PROP()
-         RETURN () (* no return value *)
-         SEP (
-            data_at sh (t_run) (original_freelist_pointer) new_head; (* the new head should point to the original freelist pointer *)
-            data_at sh t_struct_kmem (Vint (Int.repr xx), new_head) (gv _kmem) (** the top of the freelist should point to the new head *)
-            ).
-   
-   Definition call_kfree1_if_1_spec := 
-      DECLARE _call_kfree1_if_1
-         WITH sh : share, new_head:val, original_freelist_pointer:val, xx:Z, gv:globals
-         PRE [ tptr tvoid]
-            PROP(writable_share sh; is_pointer_or_null original_freelist_pointer) 
-            PARAMS (new_head) GLOBALS(gv)
-            SEP (data_at sh (t_run) nullval new_head;
-            data_at sh t_struct_kmem (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem) 
-            )
-         POST [ tvoid ]
-            PROP()
-            RETURN () 
-            SEP (
-               if (eq_dec new_head nullval) then
-               data_at sh (t_run) nullval new_head * 
-               data_at sh t_struct_kmem (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem) 
-               else 
-               data_at sh (t_run) (original_freelist_pointer) new_head *
-               data_at sh t_struct_kmem (Vint (Int.repr xx), new_head) (gv _kmem) 
-               ).
-
 (************** pointer comparison **************'*)
 
 
@@ -93,38 +19,6 @@ Definition pointer_cmp_bool (p q : val) (cmp : comparison) : bool :=
    eq_dec (pointer_comparison p q cmp) (Vint (Int.repr 1)).
 Definition pointer_le_bool (p q : val) : bool :=
    pointer_cmp_bool p q Cle.
-              
-
-Definition freerange_no_loop_no_add_spec :=
-   DECLARE _freerange_no_loop_no_add
-      WITH sh : share, new_head : val, pa_end : val, 
-            original_freelist_pointer : val, xx : Z, gv : globals
-      PRE [ tptr tvoid, tptr tvoid ]
-         PROP (
-            writable_share sh;
-            is_pointer_or_null original_freelist_pointer
-         ) 
-         PARAMS (new_head; pa_end) GLOBALS (gv)
-         SEP (
-            denote_tc_test_order new_head pa_end &&
-            (data_at sh (t_run) nullval new_head *
-            data_at sh t_struct_kmem 
-               (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem))
-         )
-      POST [ tvoid ]
-         PROP ()
-         RETURN ()
-         SEP (
-            (*denote_tc_test_order new_head pa_end **)
-            if pointer_le_bool new_head pa_end then
-               data_at sh (t_run) original_freelist_pointer new_head *
-               data_at sh t_struct_kmem 
-                  (Vint (Int.repr xx), new_head) (gv _kmem) 
-            else
-               data_at sh (t_run) nullval new_head * 
-                  data_at sh t_struct_kmem 
-                     (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem) 
-         ).
 
 (************************ freelist rep  *************************)
 Fixpoint freelistrep (sh: share) (n: nat) (p: val) : mpred :=
@@ -194,25 +88,6 @@ Proof.
 Qed.
 
 Definition kfree1_freelist_spec := 
- DECLARE _kfree1
-   WITH sh : share, new_head:val, original_freelist_pointer:val, xx:Z, gv:globals, n : nat
-   PRE [ tptr tvoid]
-      PROP(writable_share sh; is_pointer_or_null original_freelist_pointer) 
-      PARAMS (new_head) GLOBALS(gv)
-      SEP (data_at sh (t_run) nullval new_head; 
-      freelistrep sh n original_freelist_pointer;
-      data_at sh t_struct_kmem (Vint (Int.repr xx), original_freelist_pointer) (gv _kmem) 
-      )
-   POST [ tvoid ]
-      PROP()
-      RETURN () 
-      SEP (
-         freelistrep sh n original_freelist_pointer; (* TODO: maybe it makes sense to have head here?*)
-         data_at sh (t_run) (original_freelist_pointer) new_head;
-         data_at sh t_struct_kmem (Vint (Int.repr xx), new_head) (gv _kmem)
-         ).
-
-Definition kfree1_freelist_spec' := 
    DECLARE _kfree1
       WITH sh : share, new_head:val, original_freelist_pointer:val, xx:Z, gv:globals, n : nat
       PRE [ tptr tvoid]
@@ -234,7 +109,7 @@ Definition kfree1_freelist_spec' :=
 
 (************** pointer comparison and freelist **************'*)
 
-Definition freerange_no_loop_no_add_spec' :=
+Definition freerange_no_loop_no_add_spec :=
    DECLARE _freerange_no_loop_no_add
       WITH sh : share, new_head : val, pa_end : val, 
             original_freelist_pointer : val, xx : Z, gv : globals, n:nat
@@ -270,35 +145,12 @@ Definition freerange_no_loop_no_add_spec' :=
 
 (************************ Gprog  *************************)
 Definition Gprog : funspecs := [
-kfree1_spec; kfree1_1_spec ; 
-call_kfree1_spec; 
-call_kfree1_if_1_spec; freerange_no_loop_no_add_spec;
-kfree1_freelist_spec; kfree1_freelist_spec';
-freerange_no_loop_no_add_spec'
+   kfree1_freelist_spec;
+   freerange_no_loop_no_add_spec
 ].
 
 
 (************************ Proofs  *************************)
-
-Lemma body_kfree1: semax_body Vprog Gprog f_kfree1 kfree1_spec.
-Proof. start_function. repeat forward. entailer!. Qed.
-
-Lemma body_kfree1_1: semax_body Vprog Gprog f_kfree1 kfree1_1_spec.
-Proof. start_function. Intros. repeat forward. entailer!. Qed.
-
-Lemma body_call_kfree1: semax_body Vprog Gprog f_call_kfree1 call_kfree1_spec.
-Proof. start_function. forward_call. entailer!. Qed.
-
-Lemma body_call_kfree1_if_1_spec: semax_body Vprog Gprog f_call_kfree1_if_1 call_kfree1_if_1_spec.
-Proof.
-start_function.
-forward_if.
-- forward_call.
-  entailer!.
-  destruct (eq_dec new_head nullval); entailer!.
-- forward. entailer!. destruct (eq_dec nullval nullval); entailer!.
-Qed.
-
 
 Lemma cmp_le_is_either_0_or_1 : forall p q i,
    sem_cmp_pp Cle p q = Some (Vint i) ->
@@ -316,40 +168,8 @@ destruct (Val.cmplu_bool true2 Cle p q).
 - inversion H1.
 Qed.
 
-Lemma body_freerange_no_loop_no_add: semax_body Vprog Gprog f_freerange_no_loop_no_add freerange_no_loop_no_add_spec.
-Proof. start_function.
-forward_if. 
-   -apply andp_left1. entailer!.
-   -forward_call (sh, new_head, original_freelist_pointer, xx, gv).
-      +apply andp_left2. entailer!.
-      +entailer. destruct (pointer_le_bool new_head pa_end) eqn:e; try discriminate; try contradiction. 
-         * entailer!. 
-         * unfold pointer_le_bool in e.
-           unfold pointer_cmp_bool in e. 
-           unfold pointer_comparison in e.
-           destruct (sem_cmp_pp Cle new_head pa_end) eqn:e1. 
-           --destruct v; try discriminate; try contradiction.
-             apply typed_true_tint_Vint in H0.
-             exfalso; apply H0.
-             apply cmp_le_is_either_0_or_1 in e1. destruct e1; auto.
-             rewrite H5 in e.
-             simpl in e. inversion e.
-           --entailer!.
-   - forward. entailer. destruct (pointer_le_bool new_head pa_end) eqn:e1.
-      + destruct (sem_cmp_pp Cle new_head pa_end ) eqn:e2; try contradiction; try discriminate.
-        destruct v; try discriminate; try contradiction.
-        apply typed_false_tint_Vint in H0.
-        rewrite H0 in e2. unfold pointer_le_bool in e1. unfold pointer_cmp_bool in e1.
-        unfold pointer_comparison in e1.
-        rewrite e2 in e1. inversion e1.
-      + apply andp_left2. entailer!.
-Qed.
 
-
-Lemma body_kfree1_freelist: semax_body Vprog Gprog f_kfree1 kfree1_freelist_spec.
-Proof. start_function. repeat forward. entailer!. Qed.
-
-Lemma body_kfree1_freelist': semax_body Vprog Gprog f_kfree1 kfree1_freelist_spec'.
+Lemma body_kfree1_freelist': semax_body Vprog Gprog f_kfree1 kfree1_freelist_spec.
 Proof. start_function. Intros. repeat forward. entailer. 
        induction n. 
        - assert (original_freelist_pointer = nullval). {
@@ -360,12 +180,11 @@ Proof. start_function. Intros. repeat forward. entailer.
          Exists next_orig. entailer. fold freelistrep. entailer!.
 Qed.
 
-
-(*Lemma body_freerange_no_loop_no_add': semax_body Vprog Gprog f_freerange_no_loop_no_add freerange_no_loop_no_add_spec'.
+Lemma body_freerange_no_loop_no_add: semax_body Vprog Gprog f_freerange_no_loop_no_add freerange_no_loop_no_add_spec.
 Proof. start_function.
 forward_if. 
    -apply andp_left1. entailer!.
-   -forward_call (sh, new_head, original_freelist_pointer, xx, gv). (* why not call with *)
+   -forward_call (sh, new_head, original_freelist_pointer, xx, gv, n).
       +apply andp_left2. entailer!.
       +entailer. destruct (pointer_le_bool new_head pa_end) eqn:e; try discriminate; try contradiction. 
          * entailer!. 
@@ -388,4 +207,4 @@ forward_if.
         unfold pointer_comparison in e1.
         rewrite e2 in e1. inversion e1.
       + apply andp_left2. entailer!.
-Qed.*)
+Qed.
