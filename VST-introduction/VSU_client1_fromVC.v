46d687494060ce8ca7f97268b1462592891105eb
Line Dejgaard <linedejgaard@gmail.com>
2025-04-03 13:14:02 +0200

    set some of the vsu stuff up..

--- VST-introduction/VSU_client1_fromVC.v
+++ VST-introduction/VSU_client1_fromVC.v
@@ -5,27 +5,30 @@ Require Import VST.floyd.proofauto.
 Require Import VST.floyd.VSU.
 Require Import VC.client1.
 Require Import VC.Specs_only.
+Require Import VC.helper.
 #[export] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 
 (** As usual, we define representation relations.  First, for the free list,
     which is just a linked list much as in [VSU_stack].*)
 
-Definition tcell := Tstruct _run noattr.
+Definition t_run := Tstruct _run noattr.
 
-Fixpoint freelistrep (n: nat) (p: val) : mpred :=
+
+(************************ freelistrep *********************************)
+Fixpoint freelistrep (sh: share) (n: nat) (p: val) : mpred :=
  match n with
- | S n' => EX y: val, 
-        !! malloc_compatible (sizeof tcell) p && 
-        data_at Ews tcell  (y, (Vundef, (Vundef, Vundef))) p *
-        freelistrep n' y
+ | S n' => EX next: val, 
+        !! malloc_compatible (sizeof t_run) p &&  (* p is compatible with a memory block of size sizeof theader. *)
+        data_at sh t_run next p * (* at the location p, there is a t_run structure with the value next *)
+        freelistrep sh n' next (* "*" ensures no loops... *)
  | O => !! (p = nullval) && emp
  end.
 
-Arguments freelistrep n p : simpl never.
+Arguments freelistrep sh n p : simpl never.
 
-Lemma freelistrep_local_prop: forall n p, 
-   freelistrep n p |--  !! (is_pointer_or_null p /\ (n=0<->p=nullval) /\ (n>0<->isptr p))%nat.
-  Proof.
+Lemma freelistrep_local_prop: forall sh n p, 
+   freelistrep sh n p |--  !! (is_pointer_or_null p /\ (n=0<->p=nullval) /\ (n>0<->isptr p))%nat.
+Proof.
   intros.
   induction n as [| n' IH].
   - unfold freelistrep. entailer!. split; auto.
@@ -34,20 +37,47 @@ Lemma freelistrep_local_prop: forall n p,
   - unfold freelistrep. destruct p; Intro y; entailer!. split.
     + split; intros; inversion H2.
     + split; intros; auto. try lia. 
-  Qed.
+   Qed.
 #[export] Hint Resolve freelistrep_local_prop : saturate_local.
 
 Lemma freelistrep_valid_pointer:
-  forall n p,
-   freelistrep n p |-- valid_pointer p.
+  forall sh n p, 
+  readable_share sh ->
+   freelistrep sh n p |-- valid_pointer p.
 Proof.
-   intros. destruct n.
-   - unfold freelistrep. entailer!.
-   - unfold freelistrep. Intro y; entailer.
- Qed.
-
+  intros. destruct n.
+  - unfold freelistrep. entailer!.
+  - unfold freelistrep. Intro y; entailer.
+Qed. 
 #[export] Hint Resolve freelistrep_valid_pointer : valid_pointer.
-(** [] *)
+
+
+Lemma freelistrep_null: forall sh n x,
+       x = nullval ->
+       freelistrep sh n x = !! (n = O) && emp.
+Proof.
+   intros.
+   destruct n; unfold freelistrep; fold freelistrep.
+   autorewrite with norm. auto.
+   apply pred_ext.
+   Intros y. entailer. 
+   destruct n; entailer!; try discriminate H0. 
+Qed.
+
+Lemma freelistrep_nonnull: forall n sh x,
+   x <> nullval ->
+   freelistrep sh n x =
+   EX m : nat, EX next:val,
+          !! (n = S m) && !! malloc_compatible (sizeof t_run) x && data_at sh t_run next x * freelistrep sh m next.
+Proof.
+   intros; apply pred_ext.
+   - destruct n. 
+         + unfold freelistrep. entailer!.
+         + unfold freelistrep; fold freelistrep. Intros y.
+         Exists n y. entailer!.
+   - Intros m y. rewrite H0. unfold freelistrep at 2; fold freelistrep. Exists y. entailer!.
+Qed.
+
 
 (* ################################################################# *)
 (** * malloc_token *)
@@ -62,7 +92,7 @@ Proof.
    malloc_token represents.  But in this implementation, all blocks
    are the same size, so there's no need for such a header.
   - The memory-manager's free list contains blocks all of size
-   sizeof(tcell), which  is 16 bytes when [sizeof(size_t)=4] or 
+   sizeof(t_run), which  is ?? bytes when [sizeof(size_t)=4] or 
    32 bytes when [sizeof(size_t)=8].  When [malloc(7)] splits a
    block into two pieces, the malloc_token represents the
    second piece, the portion of the block between offset 7 and the end.
@@ -72,10 +102,10 @@ Proof.
    the two parts of the split block. *)
  
 Definition malloc_token_sz (sh: share) (n: Z) (p: val) : mpred := 
-  !! (field_compatible tcell [] p 
-      /\ malloc_compatible (sizeof tcell) p
-      /\ 0 <= n <= sizeof tcell) 
- &&  memory_block Ews (sizeof tcell - n) (offset_val n p).
+  !! (field_compatible t_run [] p 
+      /\ malloc_compatible (sizeof t_run) p
+      /\ 0 <= n <= sizeof t_run) 
+ &&  memory_block Ews (sizeof t_run - n) (offset_val n p).
 
 (** **** Exercise: 2 stars, standard (malloc_token_properties) *)
 Lemma malloc_token_sz_valid_pointer:
@@ -110,61 +140,7 @@ Definition N : Z := proj1_sig (opaque_constant 80000).
 Definition N_eq : N=_ := proj2_sig (opaque_constant _).
 #[export] Hint Rewrite N_eq : rep_lia.
 
-(* ----------------------------------------------------------------- *)
-(** *** Digression (feel free to skip this) *)
 
-Module Digression.
-(** Suppose someone changes the source program stdlib2.c,  putting a
-  different constant than 80000.  You would like your verification 
-  script to automatically adjust.    We will revise the line,
-  [Definition N : Z := ...]  to find the constant automagically.
-
-  Step one is to find the constant.  You can look in stdlib2.v, which
-  is produced by CompCert clightgen from stdlib2.c, for the variable
-  definition v_pool.  And now look where the number 80000 appears
-  in [gvar_info(v_pool)]: *)
-Compute gvar_info (stdlib2.v_pool).
-  (*   = Tarray
-         (Tstruct 57 {| attr_volatile := false; attr_alignas := None |})
-         80000
-         {| attr_volatile := false; attr_alignas := None |}
-     : type *)
-
-(** It's easy to extract that number automatically: *)
-
-Compute match gvar_info (stdlib2.v_pool) with Tarray _ n _ => n | _ => 0 end.
-  (*  = 80000: Z *)
-
-(** The following definition of [N] won't work well, because N will 
-  not be a constant, it'll be a match expression: *)
-Definition N' := match gvar_info (stdlib2.v_pool) with Tarray _ n _ => n | _ => 0 end.
-Print N'.  (* N' = match gvar_info v_pool with Tarray _ n _ => n | _ => 0 end *)
-
-(** So instead, use this Coq trick: *)
-
-Definition N'' := 
-  ltac:(let x := constr:(match gvar_info (stdlib2.v_pool) with
-                         | Tarray _ n _ => n
-			 | _ => 0 end)
-        in let x := eval compute in x in exact x).
-Print N''. (* N'' = 80000 : Z *)
-
-(** Now, throw away N'' and we'll combine the two tricks together:
- - [opaque_constant] to define a constant that won't unfold except 
-    by explicit rewriting; and
- - extract the value of the constant from the program itself. *)
-Definition N := proj1_sig (opaque_constant 
-                            (ltac:(let x := constr:(match gvar_info (stdlib2.v_pool) with
-                                          Tarray _ n _ => n | _ => 0 end)
-                                    in let x := eval compute in x in exact x))).
-Definition N_eq : N=_ := proj2_sig (opaque_constant _).
-#[export] Hint Rewrite N_eq : rep_lia.
-Check N_eq.  (* : N = 80000 *)
-
-End Digression.
-
-(* ----------------------------------------------------------------- *)
-(** *** End of digression. Aren't you glad you skipped it? *)
 
 (* ################################################################# *)
 (** * Defining the mem_mgr APD *)
@@ -174,13 +150,10 @@ End Digression.
   [pool], [pool_index], and [freelist]. *)
 
 Definition mem_mgr (gv: globals) : mpred :=
- EX i: Z, EX p: val, EX frees: nat,
-  !! (0 <= i <= N) &&
-  data_at Ews tint (Vint (Int.repr i)) (gv _pool_index) *
-  data_at_ Ews (tarray tcell (N-i))
-     (field_address0 (tarray tcell N) [ArraySubsc i]  (gv _pool)) *
-  data_at Ews (tptr tcell) p (gv _freelist) *
-  freelistrep frees p.
+    EX sh:share, EX head:val, EX xx:Z, EX n:nat,
+    data_at sh t_struct_kmem (Vint (Int.repr xx), head) (gv _kmem) *
+    freelistrep sh n head.
+
 
 Definition M : MallocFreeAPD := 
     Build_MallocFreeAPD mem_mgr malloc_token_sz
@@ -192,52 +165,23 @@ Definition M : MallocFreeAPD :=
   Definition MF_ASI: funspecs := MallocFreeASI M.
   Definition MF_imported_specs:funspecs :=  nil.
   Definition MF_internal_specs: funspecs := MF_ASI.
-  Definition MF_globals gv : mpred:= Spec_stdlib.mem_mgr M gv.
-  Definition MFVprog : varspecs. mk_varspecs stdlib2.prog. Defined.
+  Definition MF_globals gv : mpred:= Specs_only.mem_mgr M gv.
+  Definition MFVprog : varspecs. mk_varspecs client1.prog. Defined.
   Definition MFGprog: funspecs := MF_imported_specs ++ MF_internal_specs.
 
 (** **** Exercise: 3 stars, standard (stdlib2_body_malloc) *)
 
-Lemma body_malloc: semax_body MFVprog MFGprog f_malloc (malloc_spec_sz M).
+Lemma body_kalloc: semax_body MFVprog MFGprog f_kalloc1 (kalloc_spec_sz M).
 Proof.
   start_function.
-  forward_if.
-  - forward. Exists (Vlong (Int64.repr 0)). 
-    simpl. entailer!.
-  - unfold _freelist. simpl. 
-  
-
-  
-    (*forward_if.
-  + induction 
-  + induction
-
-  induction n.
-    2: {
-      forward.
-    }
-    + forward.
-   forward_if (
-    PROP (q <> nullval)
-    LOCAL (temp _q q; temp _p p; temp _i (Vint (Int.repr i)))
-    SEP (
-    if eq_dec q nullval
-    then emp
-          (* malloc_token Ews (Tstruct _cons noattr) q means there is a pointer *)  (*  data_at_ Ews (Tstruct _cons noattr) q means there asserts that the memory exists but hasn't been assigned meaningful data yet *)
-    else malloc_token Ews (Tstruct _cons noattr) q * data_at_ Ews (Tstruct _cons noattr) q; 
-    stack il p;mem_mgr gv)).*)
-  
 (* FILL IN HERE *) Admitted.
 (** [] *)
 
 (** **** Exercise: 3 stars, standard (stdlib2_body_free) *)
-Lemma body_free: semax_body MFVprog MFGprog f_free (free_spec_sz M).
+Lemma body_free: semax_body MFVprog MFGprog f_kfree1 (kfree_spec_sz M).
 (* FILL IN HERE *) Admitted.
 (** [] *)
 
-(** **** Exercise: 2 stars, standard (stdlib2_body_exit) *)
-Lemma body_exit: semax_body MFVprog MFGprog f_exit exit_spec.
-(* FILL IN HERE *) Admitted.
 (** [] *)
 
 (* ################################################################# *)
@@ -268,17 +212,17 @@ unfold MF_globals.
 (* ################################################################# *)
 (** * Constructing the Component and the VSU *)
 
-  Definition MF_Externs : funspecs := nil.
+  (*Definition MF_Externs : funspecs := nil.
 
 Definition MallocFreeVSU: @VSU NullExtension.Espec
          MF_Externs MF_imported_specs ltac:(QPprog prog) MF_ASI MF_globals.
   Proof. 
- mkVSU prog MF_internal_specs.
+    mkVSU prog MF_internal_specs.
     - solve_SF_internal body_malloc.
     - solve_SF_internal body_free.
     - solve_SF_internal body_exit.
     - apply initialize; auto.
-Qed.
+Qed.*)
 
 (* ================================================================= *)
 (** ** Next Chapter: [VSU_main2] *)
